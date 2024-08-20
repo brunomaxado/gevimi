f5fe587c50bdcfb09bead09750d17be42d33c432
Bruno Machado <brunomachado.1706@gmail.com>
2024-08-19 23:48:39 -0300

    adicionei o header

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Add from "./pages/Add";
-import Update from "./pages/Update";
 import Books from "./pages/Books";
+import Header from "./pages/Header";
 import Produto from "./pages/Produto";
-import "./style.css"
+import "./style.css";
 import GerenciarProduto from "./pages/GerenciarProduto";
 
 
@@ -12,11 +12,11 @@ function App() {
     <div className="app">
       
       <BrowserRouter>
+      <Header />
         <Routes>
           <Route path="/" element={<Books />} />
-          <Route path="/add" element={<Add />} />
           <Route path="/produto" element={<Produto />} />
-          <Route path="/update/:id" element={<Update />} />
+        
           <Route path="/gerenciarproduto/:id_produto" element={<GerenciarProduto />} />
         </Routes>
       </BrowserRouter>
---
+++ client/src/pages/Header.jsx
@@ -0,0 +1,26 @@
+// Header.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'; // Importa o arquivo de estilo
+
+const Header = () => {
+  return (
+    <header className="header">
+      <nav className="nav">
+        <ul className="nav-list">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">Books</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/produto" className="nav-link">Produto</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/gerenciarproduto/:id_produto" className="nav-link">Gerenciar Produto</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
