7deba071b9914a1fc4271475d5da1dc3c8077f3c
Bruno Machado <brunomachado.1706@gmail.com>
2024-09-12 18:05:19 -0300

    sem login sem header

--- client/src/App.js
+++ client/src/App.js
@@ -9,44 +9,43 @@ import Cliente from "./pages/Cliente";
 import ReadCliente from "./pages/readCliente";
 import EditarCliente from "./pages/editarCliente";
 import Login from "./pages/login";
-import Home from "./pages/home" ;
+import Home from "./pages/home";
 import Pedido from "./pages/Pedido";
 import Footer from "./pages/Footer";
 import Register from "./pages/register";
-import React, { useContext } from "react";
+import React from "react";
 import useAuth from "./context/useAuth";
-const Private = ({ Component }) => {
-  const { currentUser } = useAuth(); // Chame o hook useAuth corretamente
 
-  // Verifique se currentUser está presente para determinar se o usuário está autenticado
+const Private = ({ Component }) => {
+  const { currentUser } = useAuth();
+
   return currentUser ? <Component /> : <Login />;
 };
 
-
 function App() {
-    return (
-        <div className="app">
-            <BrowserRouter>
-                <Header />
-                <Routes>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/pedido" element={<Pedido />} />
-                    <Route path="/viewProduto" element={<Private Component={Books} />} />
-                    <Route path="/produto" element={<Private Component={Produto} />} />
-                    <Route path="/gerenciarproduto/:id_produto" element={<Private Component={GerenciarProduto} />} />
-                    <Route path="/cliente" element={<Private Component={Cliente} />} />
-                    <Route path="/categoria" element={<Private Component={Categoria} />} />
-                    <Route path="/readCliente" element={<Private Component={ReadCliente} />} />
-                    <Route path="/editarCliente/:id_cliente" element={<Private Component={EditarCliente} />} />
-                    
-                    <Route path="/" element={<Home />} />
-                </Routes>
-            </BrowserRouter>
-            <Footer />
+  const { currentUser } = useAuth(); // Obtenha o usuário atual do contexto de autenticação
 
-        </div>
-    );
+  return (
+    <div className="app">
+      <BrowserRouter>
+        {currentUser && <Header />} {/* Exibe o Header somente se houver um usuário autenticado */}
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/pedido" element={<Pedido />} />
+          <Route path="/viewProduto" element={<Private Component={Books} />} />
+          <Route path="/produto" element={<Private Component={Produto} />} />
+          <Route path="/gerenciarproduto/:id_produto" element={<Private Component={GerenciarProduto} />} />
+          <Route path="/cliente" element={<Private Component={Cliente} />} />
+          <Route path="/categoria" element={<Private Component={Categoria} />} />
+          <Route path="/readCliente" element={<Private Component={ReadCliente} />} />
+          <Route path="/editarCliente/:id_cliente" element={<Private Component={EditarCliente} />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
