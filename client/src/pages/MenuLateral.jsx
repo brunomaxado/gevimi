776ccbea96028f6f6bca591fabebf439a554bea6
Is4bele <isacorreia.ilc@gmail.com>
2024-10-17 19:43:07 -0300

    css e menu

--- client/src/pages/MenuLateral.jsx
+++ client/src/pages/MenuLateral.jsx
@@ -1,29 +1,18 @@
 import { Link } from 'react-router-dom';
 import { AuthContext } from "../context/authContext";
-import React, { useContext, useEffect, useState } from "react";
-import axios from "axios";
+import React, { useContext, useEffect } from "react";
 
 const Menu = () => {
     const { currentUser, logout } = useContext(AuthContext);
-    const [openDropdown, setOpenDropdown] = useState(null);
 
     useEffect(() => {
         const list = document.querySelectorAll('.list');
         function activeLink() {
             list.forEach((item) => item.classList.remove('active'));
             this.classList.add('active');
-            if (this.querySelector('.dropdown-content')) {
-                this.classList.toggle('dropdown-active');
-            } else {
-                list.forEach((item) => item.classList.remove('dropdown-active'));
-            }
         }
         list.forEach((item) => item.addEventListener('click', activeLink));
     }, []);
-
-    const toggleDropdown = (index) => {
-        setOpenDropdown(openDropdown === index ? null : index);
-    };
 
     return (
         <div className="navigation">
@@ -35,38 +24,32 @@ const Menu = () => {
                     </Link>
                 </li>
                 <li className="list">
-                    <Link to="#">
+                    <Link to="/categoria">
                         <span className="icon"><ion-icon name="bookmarks-outline"></ion-icon></span>
                         <span className="title">Categorias</span>
-                        <span className="icon flecha"><ion-icon name="chevron-down-outline"></ion-icon></span>
                     </Link>
                 </li>
-                <li className="list" onClick={() => toggleDropdown(1)}>
+                <li className="list">
                     <Link to="#">
                         <span className="icon"><ion-icon name="people-outline"></ion-icon></span>
                         <span className="title">Clientes</span>
                         <span className="icon flecha"><ion-icon name="chevron-down-outline"></ion-icon></span>
                     </Link>
-                    {openDropdown === 1 && (
-                        <div className="dropdown-content">
-                            <Link to="/"><span className="title">Novo cliente</span></Link>
-                            <Link to="/"><span className="title">Listar clientes</span></Link>
-                        </div>
-                    )}
+                    <div className="dropdown-content">
+                        <Link to="/cliente"><span className="title">Novo cliente</span></Link>
+                        <Link to="/readCliente"><span className="title">Listar clientes</span></Link>
+                    </div>
                 </li>
-                
-                <li className="list"  onClick={() => toggleDropdown(2)}>
+                <li className="list">
                     <Link to="#">
                         <span className="icon"><ion-icon name="pricetags-outline"></ion-icon></span>
                         <span className="title">Produtos</span>
                         <span className="icon flecha"><ion-icon name="chevron-down-outline"></ion-icon></span>
                     </Link>
-                    {openDropdown === 2 && (
                     <div className="dropdown-content">
                         <Link to="/produto"><span className="title">Novo produto</span></Link>
                         <Link to="/viewProduto"><span className="title">Listar produto</span></Link>
                     </div>
-                     )}
                 </li>
                 <li className="list">
                     <Link to="#">
@@ -83,7 +66,7 @@ const Menu = () => {
                 <li className="list">
                     <Link to="/" onClick={logout}>
                         <span className="icon"><ion-icon name="log-out-outline"></ion-icon></span>
-                        <span className="title" >Sair</span>
+                        <span className="title">Sair</span>
                     </Link>
                 </li>
             </ul>
