8c2e499a5c44d8853f2f7599c7e98fb1b2bf01e4
Is4bele <isacorreia.ilc@gmail.com>
2024-10-28 12:56:11 -0300

    style do menu lateral e modificação de algumas funcionalidades do menu

--- client/src/pages/MenuLateral.jsx
+++ client/src/pages/MenuLateral.jsx
@@ -1,9 +1,21 @@
 import { Link } from 'react-router-dom';
 import { AuthContext } from "../context/authContext";
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 const Menu = () => {
     const { currentUser, logout } = useContext(AuthContext);
+    const [isOpen, setIsOpen] = useState(false);
+    const [showDropdowns, setShowDropdowns] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+        setShowDropdowns(!showDropdowns);
+    };
+
+    const handleLinkClick = () => {
+        setIsOpen(false);
+        setShowDropdowns(false); // Fecha todos os dropdowns
+    };
 
     useEffect(() => {
         const list = document.querySelectorAll('.list');
@@ -15,64 +27,100 @@ const Menu = () => {
     }, []);
 
     return (
-        <div className="navigation">
+        <div className={`navigation ${isOpen ? 'open' : ''}`}>
+            <button onClick={toggleMenu} className="menu-button">
+                <ion-icon name="menu-outline"></ion-icon>
+            </button>
             <ul>
                 <li className="list active">
-                    <Link to="/home">
+                    <Link to="/home" onClick={handleLinkClick}>
                         <span className="icon"><ion-icon name="home-outline"></ion-icon></span>
                         <span className="title">Início</span>
                     </Link>
                 </li>
                 <li className="list">
-                    <Link to="/categoria">
+                    <Link to="/categoria" onClick={handleLinkClick}>
                         <span className="icon"><ion-icon name="bookmarks-outline"></ion-icon></span>
                         <span className="title">Categorias</span>
                     </Link>
                 </li>
                 <li className="list">
-                    <Link to="#">
+                    <Link to="#" onClick={handleLinkClick}>
                         <span className="icon"><ion-icon name="person-outline"></ion-icon></span>
                         <span className="title">Usuário</span>
                         <span className="icon flecha"><ion-icon name="chevron-down-outline"></ion-icon></span>
                     </Link>
-                    <div className="dropdown-content">
-                        <Link to="/register"><span className="title">Registrar</span></Link>
-                        <Link to="/alterarSenha"><span className="title">Editar</span></Link>
-                        <Link to="/readUsuario"><span className="title">Listar</span></Link>
+                    <div className={`dropdown-content ${showDropdowns ? 'show' : ''}`}>
+                        <Link to="/register" onClick={handleLinkClick}>
+                            <span className="icon-menor"><ion-icon name="chevron-forward-outline"></ion-icon></span>
+                            <span className="title">Registrar</span>
+                        </Link>
+                        <Link to="/alterarSenha" onClick={handleLinkClick}>
+                            <span className="icon-menor"><ion-icon name="chevron-forward-outline"></ion-icon></span>
+                            <span className="title">Editar</span>
+                        </Link>
+                        <Link to="/readUsuario" onClick={handleLinkClick}>
+                            <span className="icon-menor"><ion-icon name="chevron-forward-outline"></ion-icon></span>
+                            <span className="title">Listar</span>
+                        </Link>
                     </div>
                 </li>
                 <li className="list">
-                    <Link to="/readCliente">
+                    <Link to="#" onClick={handleLinkClick}>
                         <span className="icon"><ion-icon name="people-outline"></ion-icon></span>
                         <span className="title">Clientes</span>
                         <span className="icon flecha"><ion-icon name="chevron-down-outline"></ion-icon></span>
                     </Link>
-                    <div className="dropdown-content">
-                        <Link to="/cliente"><span className="title">Novo cliente</span></Link>
-                        <Link to="/readCliente"><span className="title">Listar clientes</span></Link>
+                    <div className={`dropdown-content ${showDropdowns ? 'show' : ''}`}>
+                        <Link to="/cliente" onClick={handleLinkClick}>
+                            <span className="icon-menor"><ion-icon name="chevron-forward-outline"></ion-icon></span>
+                            <span className="title">Novo cliente</span>
+                        </Link>
+                        <Link to="/readCliente" onClick={handleLinkClick}>
+                            <span className="icon-menor"><ion-icon name="chevron-forward-outline"></ion-icon></span>
+                            <span className="title">Listar clientes</span>
+                        </Link>
                     </div>
                 </li>
                 <li className="list">
-                    <Link to="/viewProduto">
+                    <Link to="#" onClick={handleLinkClick}>
                         <span className="icon"><ion-icon name="pricetags-outline"></ion-icon></span>
                         <span className="title">Produtos</span>
                         <span className="icon flecha"><ion-icon name="chevron-down-outline"></ion-icon></span>
                     </Link>
-                    <div className="dropdown-content">
-                        <Link to="/produto"><span className="title">Novo produto</span></Link>
-                        <Link to="/viewProduto"><span className="title">Listar produto</span></Link>
+                    <div className={`dropdown-content ${showDropdowns ? 'show' : ''}`}>
+                        <Link to="/produto" onClick={handleLinkClick}>
+                            <span className="icon-menor"><ion-icon name="chevron-forward-outline"></ion-icon></span>
+                            <span className="title">Novo produto</span>
+                        </Link>
+                        <Link to="/viewProduto" onClick={handleLinkClick}>
+                            <span className="icon-menor"><ion-icon name="chevron-forward-outline"></ion-icon></span>
+                            <span className="title">Listar produto</span>
+                        </Link>
                     </div>
                 </li>
                 <li className="list">
-                    <Link to="/readPedido">
+                    <Link to="#" onClick={handleLinkClick}>
                         <span className="icon"><ion-icon name="storefront-outline"></ion-icon></span>
                         <span className="title">Pedidos</span>
                         <span className="icon flecha"><ion-icon name="chevron-down-outline"></ion-icon></span>
                     </Link>
-                    <div className="dropdown-content">
-                        <Link to="/pedido"><span className="title">Novo pedido</span></Link>
-                        <Link to="/readPedido"><span className="title">Listar pedidos</span></Link>
+                    <div className={`dropdown-content ${showDropdowns ? 'show' : ''}`}>
+                        <Link to="/pedido" onClick={handleLinkClick}>
+                            <span className="icon-menor"><ion-icon name="chevron-forward-outline"></ion-icon></span>
+                            <span className="title">Novo pedido</span>
+                        </Link>
+                        <Link to="/readPedido" onClick={handleLinkClick}>
+                            <span className="icon-menor"><ion-icon name="chevron-forward-outline"></ion-icon></span>
+                            <span className="title">Listar pedidos</span>
+                        </Link>
                     </div>
+                </li>
+                <li className="list">
+                    <Link to="/estatistica" onClick={handleLinkClick}>
+                        <span className="icon"><ion-icon name="stats-chart-outline"></ion-icon></span>
+                        <span className="title">Estatísticas</span>
+                    </Link>
                 </li>
                 <li className="list">
                     <Link to="#">
@@ -81,13 +129,7 @@ const Menu = () => {
                     </Link>
                 </li>
                 <li className="list">
-                    <Link to="/estatistica">
-                        <span className="icon"><ion-icon name="stats-chart-outline"></ion-icon></span>
-                        <span className="title">Estatísticas</span>
-                    </Link>
-                </li>
-                <li className="list">
-                    <Link to="/" onClick={logout}>
+                    <Link to="/" onClick={() => { logout(); handleLinkClick(); }}>
                         <span className="icon"><ion-icon name="log-out-outline"></ion-icon></span>
                         <span className="title">Sair</span>
                     </Link>
