eb78193d45f8378572eff3785bd1a638f2dbd705
Is4bele <isacorreia.ilc@gmail.com>
2024-11-09 19:52:43 -0300

    help usuário cadastro

--- client/src/components/modalHelpNovoUsuario.jsx
+++ client/src/components/modalHelpNovoUsuario.jsx
@@ -15,40 +15,25 @@ const ModalHelpNovoUsuario = ({ isOpen, onRequestClose }) => {
         </div>
         <div className="custom-modal-body">
           <div>
-            <h5><b>:</b></h5>
+            <h5><b>Cadastrar novo usuário:</b></h5>
             <dl>
-              <dt>:</dt>
-              <dd>- </dd>
+              <dt>Nome:</dt>
+              <dd>- Digite o nome do usuário, pode ser o primeiro nome e as iniciais.</dd>
+              <dt>Login:</dt>
+              <dd>- Digite o primeiro nome do usuário, ele usará esse nome para entrar no sistema.</dd>
+              <dt>Senha:</dt>
+              <dd>- Digite a senha para esse usuário, pode incluir letras e números por exemplo.</dd>
+              <dt>Administrador:</dt>
+              <dd>- Defina as permissões desse usuário, se for administrador ele poderá gerenciar outros
+                usuários.
+              </dd>
               <dt>Botão Confirmar:</dt>
               <dd>
-
+              - Ao conferir os dados e entender que estão corretos, deve-se clicar no botão "Confirmar".
+                 Assim, se o cadastro foi bem-sucedido, um pop-up verde claro aparecerá na parte superior
+                  da tela avisando que foi bem-sucedido, caso contrário, um aviso indicando que não foi possível
+                  aparecerá. Se bem-sucedido, será redirecionado para a lista de todas os usuários cadastrados.
               </dd>
-            </dl>
-          </div>
-
-          <div>
-            <h5><b>:</b></h5>
-            <dl>
-              <dt>:</dt>
-              <dd>- .</dd>
-              <dt>:</dt>
-              <dd>- .</dd>
-            </dl>
-          </div>
-
-          <div>
-            <h5><b>:</b></h5>
-            <dl>
-              <dt>:</dt>
-              <dd>- .</dd>
-            </dl>
-          </div>
-
-          <div>
-            <h5><b>:</b></h5>
-            <dl>
-              <dt>:</dt>
-              <dd>- .</dd>
             </dl>
           </div>
         </div>
