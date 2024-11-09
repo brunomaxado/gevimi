2afc6a6d98bcecf84104fbf0372aeb9f5af413e2
Is4bele <isacorreia.ilc@gmail.com>
2024-11-09 14:23:16 -0300

    help read produto

--- client/src/components/modalHelpReadProduto.jsx
+++ client/src/components/modalHelpReadProduto.jsx
@@ -15,40 +15,44 @@ const ModalHelpReadProduto = ({ isOpen, onRequestClose }) => {
         </div>
         <div className="custom-modal-body">
           <div>
-            <h5><b>:</b></h5>
+            <h5><b>Pesquisar:</b></h5>
             <dl>
-              <dt>:</dt>
-              <dd>- </dd>
-              <dt>Botão Confirmar:</dt>
+              <dt>Caixa de pesquisa:</dt>
+              <dd>- Pesquise um produto por seu nome.</dd>
+              <dt>Filtrar por categoria:</dt>
               <dd>
-
+                - Veja todos os produtos de uma categoria específica ao selecionar no filtro.
+              </dd>
+              <dt>Limpar filtros:</dt>
+              <dd>
+                - Limpe os resultados de pesquisa e filtros.
               </dd>
             </dl>
           </div>
 
           <div>
-            <h5><b>:</b></h5>
+            <h5><b>Novo produto:</b></h5>
             <dl>
-              <dt>:</dt>
-              <dd>- .</dd>
-              <dt>:</dt>
-              <dd>- .</dd>
+              <dt>Cadastrar produto:</dt>
+              <dd>- Clique no botão "novo produto" para ser redirecionado para a página de cadastro de produtos.</dd>
             </dl>
           </div>
 
           <div>
-            <h5><b>:</b></h5>
+            <h5><b>Deletar produto:</b></h5>
             <dl>
-              <dt>:</dt>
-              <dd>- .</dd>
+              <dt>Ícone de deletar (lixeira):</dt>
+              <dd>- Clique no ícone para deletar o produto,
+                 só é possível deletar produto se ele não estiver associado
+                  à algum pedido.</dd>
             </dl>
           </div>
 
           <div>
-            <h5><b>:</b></h5>
+            <h5><b>Editar produto:</b></h5>
             <dl>
-              <dt>:</dt>
-              <dd>- .</dd>
+              <dt>Ícone de edição (lápis):</dt>
+              <dd>- Clique no ícone para editar o nome, preço, categoria e descrição.</dd>
             </dl>
           </div>
         </div>
