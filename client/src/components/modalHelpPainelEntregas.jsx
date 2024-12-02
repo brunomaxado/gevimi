9caa202475933592f2ad4f05a0dcdcf353f1a822
Is4bele <isacorreia.ilc@gmail.com>
2024-12-02 16:12:34 -0300

    aaa

--- client/src/components/modalHelpPainelEntregas.jsx
+++ client/src/components/modalHelpPainelEntregas.jsx
@@ -8,7 +8,7 @@ const ModalHelpPainelEntregas = ({ isOpen, onRequestClose }) => {
     <div className="custom-modal-overlay">
       <div className="custom-modal">
         <div className="custom-modal-header">
-          <h2>Ajuda na página de painel de entregas.</h2>
+          <h2>Ajuda na página inicial.</h2>
           <button className="custom-modal-close" onClick={onRequestClose}>
             &times;
           </button>
@@ -24,36 +24,15 @@ const ModalHelpPainelEntregas = ({ isOpen, onRequestClose }) => {
             </div>
 
             <div>
-              <h5><b>Pesquisar:</b></h5>
+              <h5><b>Menu lateral:</b></h5>
               <dl>
-                <dt>Barra de pesquisa:</dt>
-                <dd>-  Na caixa de pesquisa, é possível pesquisar o nome do cliente.</dd>
-                <dt>Status:</dt>
-                <dd>- É possível filtrar os pedidos por estado: finalizado(quando o pedido foi entregue),
-                  em andamento(quando o pedido está sendo preparado ou em espera até o dia) ou
-                  pendente(quando o pedido não foi entregue e está atrasado).</dd>
-                <dt>Tipo:</dt>
-                <dd>- É possível filtrar os pedidos por tipo: Entrega(quando os funcionários fazem a entrega),
-                  Entrega Ifood(quando a entrega é feita por terceiros pelo aplicativo do Ifood),
-                  Retirada(quando o cliente encomenda e retira na loja) e
-                  comum(quando o cliente compra à pronta entrega sem necessidade de encomenda).</dd>
-                <dt>Limpar filtro:</dt>
-                <dd>- Retira os filtros e limpa a caixa de pesquisa.</dd>
+
+                <dd>- no menu lateral é possível escolher o que fazer.</dd>
+
               </dl>
             </div>
 
-            <div>
-              <h5><b>Gerenciar pedidos:</b></h5>
-              <dl>
-                <dt>Finalizar:</dt>
-                <dd>- Clique no ícone para finalizar o pedido.</dd>
-                <dt>Visualizar:</dt>
-                <dd>- Clique no ícone para visualizar todas as informações do pedido.</dd>
-                <dt>Ícone de deletar (lixeira):</dt>
-                <dd>- Clique no ícone para deletar o pedido,
-                  só é possível deletar pedido se ele não estiver finalizado.</dd>
-              </dl>
-            </div>
+
           </div>
         </div>
         <div className="custom-modal-footer">
